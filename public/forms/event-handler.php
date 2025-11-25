351c3dcf073728b7c7dc17771c4a997b8e12d074
alijqadir <alijqadir@gmail.com>
2025-11-25 06:28:46 +0500

    Update event handler webhook URL

--- public/forms/event-handler.php
+++ public/forms/event-handler.php
@@ -5,7 +5,7 @@
 // - Deploy the Apps Script from static/event-invite-apps-script.md and paste the /exec URL below.
 
 const CSV_PATH = __DIR__ . '/data/event-rsvps.csv';
-const GOOGLE_WEBHOOK = 'https://script.google.com/macros/s/AKfycbxyy9hLI0ClL4Tgou9WQKTtWw_y6PW2p9VDSvlu3sbz5t1GddAgiEWhmuHqeSK9yd8B/exec';
+const GOOGLE_WEBHOOK = 'https://script.google.com/macros/s/AKfycbzNT8VS7TL1CK9GI1NXCcHz-Mu_6Cl0YAotlIK0gxKdEehgqBh4aEI9kaYdnEGBKEt3/exec';
 
 header('Content-Type: text/plain; charset=utf-8');
 if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
